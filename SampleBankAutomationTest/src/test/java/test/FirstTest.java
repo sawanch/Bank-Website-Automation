c7a0df25ec23cd78c8305b52875b1965b4070b7b
sawanch <sawanchakraborty100@gmail.com>
2023-11-22 21:59:08 -0800

    Initial Login and password check in Bank Website

---
+++ SampleBankAutomationTest/src/test/java/test/FirstTest.java
@@ -0,0 +1,35 @@
+package test;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class FirstTest {
+
+	public static void main(String[] args) {
+		
+		
+		WebDriver driver = new FirefoxDriver();
+		
+		driver.get("https://www.demo.guru99.com/V4/");
+		
+		//Find the username and enter the username
+		
+		WebElement textbox_username	= driver.findElement(By.name("uid"));
+		
+		textbox_username.sendKeys("mngr539530");
+		
+		//Password is provided
+		
+		driver.findElement(By.name("password")).sendKeys("UvavUpu");
+		
+		//login
+		
+		driver.findElement(By.xpath("//input[@type='submit']")).click();
+		
+		//driver.quit();
+
+	}
+
+}
